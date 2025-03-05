c7a1d0697a11d7e90e1e3c1a48c33da796bc65eb
saniya <mirzasaniya716@gmail.com>
2025-03-05 21:20:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.chatapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.chatapp.screens.SignInScreen
+import com.example.chatapp.ui.theme.ChatAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SignInScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/screens/SignInScreen.kt
@@ -0,0 +1,94 @@
+package com.example.chatapp.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chatapp.R
+
+@Preview(showSystemUi = true)
+@Composable
+fun SignInScreen() {
+    val brush = Brush.linearGradient(
+        listOf(
+            Color(0xFF238CDD),
+            Color(0xFF255DCC)
+        )
+    )
+    Image(
+        painter = painterResource(id = R.drawable.login_blur),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(80.dp))
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = null,
+            modifier = Modifier.size(400.dp)
+        )
+        Text(
+            text = "Universe of Conversations",
+            style = MaterialTheme.typography.displayMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF101010),
+                fontSize = 30.sp
+            )
+        )
+        Text(
+            text = "Chat, laugh and connect with new friends. Dive into a fun world where conversations flow and friendships grow!",
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
+            color = Color(0xFF101010),
+            modifier = Modifier.padding(15.dp)
+        )
+        Spacer(modifier = Modifier.height(70.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .background(brush, CircleShape)
+                .fillMaxWidth(.9f)
+                .height(60.dp),
+            colors = ButtonDefaults.buttonColors(Color.Transparent), shape = CircleShape
+        ) {
+            Text(
+                text = "Launch your journey with Google",
+                modifier = Modifier.padding(end = 4.dp),
+                color = Color.White,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Image(
+                painter = painterResource(id = R.drawable.goog_0ed88f7c),
+                contentDescription = null,
+                modifier = Modifier.scale(.6f)
+            )
+        }
+    }
+}
